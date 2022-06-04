f3749a363a939a1ba6d53568e90b95f85233f039
Serhii <sergei181989@gmail.com>
2022-06-04 22:51:37 +0300

    :sparkles: Added project theme change and saving to local storage.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 
 import './styles/index.scss'
+import { ThemeProvider } from './provider/ThemeProvider'
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+	document.getElementById('root') as HTMLElement
+)
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<BrowserRouter>
+			<ThemeProvider>
+				<App />
+			</ThemeProvider>
+		</BrowserRouter>
+	</React.StrictMode>
+)
--- src/shared/Header/index.tsx
+++ src/shared/Header/index.tsx
@@ -1,40 +1,52 @@
+import { GlobalSvgSelector } from '../../assets/icons/global/GlobalSvgSelector'
 import Select from 'react-select'
-import {GlobalSvgSelector} from "../../assets/icons/global/GlobalSvgSelector";
 import styles from './Header.module.scss'
+import { useTheme } from '../../hooks/useTheme'
+import { Theme } from '../../context/ThemeContext'
 
 
 export const Header = () => {
-  const options = [
-    {value: 'city-1', label: 'Kremenchug'},
-    {value: 'city-2', label: 'Kyiv'},
-    {value: 'city-3', label: 'Poltava'}
-  ];
+	const theme = useTheme()
 
-  const colorStyles = {
-    control: (styles: any) => ({
-      ...styles,
-      backgroundColor: 'rgba(71,147,255,0.2)',
-      width: '194px',
-      height: '37px',
-      border: 'none',
-      borderRadius: '10px',
-      zIndex: 100,
-    })
-  }
-  return (
-    <header className={styles.header}>
-      <div className={styles.wrapper}>
-        <div className={styles.logo}>
-          <GlobalSvgSelector id='header-logo'/>
-        </div>
-        <div className={styles.title}>React weather</div>
-      </div>
-      <div className={styles.wrapper}>
-        <div className={styles.change_theme}>
-          <GlobalSvgSelector id='change-theme'/>
-        </div>
-        <Select defaultValue={options[0]} options={options} styles={colorStyles}/>
-      </div>
-    </header>
-  )
+	const options = [
+		{ value: 'city-1', label: 'Kremenchug' },
+		{ value: 'city-2', label: 'Kyiv' },
+		{ value: 'city-3', label: 'Poltava' }
+	]
+
+	const colorStyles = {
+		control: (styles: any) => ({
+			...styles,
+			backgroundColor: theme.theme === Theme.DARK ? '#4f4f4f' : 'rgba(71,147,255,0.2)',
+			width: '194px',
+			height: '37px',
+			border: 'none',
+			borderRadius: '10px',
+			zIndex: 100
+		}),
+		singleValue: (styles: any) => ({
+			...styles, color: theme.theme === Theme.DARK ? '#fff' : '#000'
+		})
+	}
+
+	const changeTheme = () => {
+		theme.changeTheme(theme.theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT)
+	}
+
+	return (
+		<header className={styles.header}>
+			<div className={styles.wrapper}>
+				<div className={styles.logo}>
+					<GlobalSvgSelector id='header-logo' />
+				</div>
+				<div className={styles.title}>React weather</div>
+			</div>
+			<div className={styles.wrapper}>
+				<div className={styles.change_theme} onClick={changeTheme}>
+					<GlobalSvgSelector id='change-theme' />
+				</div>
+				<Select defaultValue={options[0]} options={options} styles={colorStyles} />
+			</div>
+		</header>
+	)
 }
