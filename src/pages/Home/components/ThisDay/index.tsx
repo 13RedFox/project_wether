784d0e2adf901567dd71439983b677947a6a20d2
Serhii <sergei181989@gmail.com>
2022-06-06 12:19:12 +0300

    :sparkles: Added getting city weather.

--- src/pages/Home/components/ThisDay/index.tsx
+++ src/pages/Home/components/ThisDay/index.tsx
@@ -1,20 +1,25 @@
 import styles from './ThisDay.module.scss'
-import {GlobalSvgSelector} from "../../../../assets/icons/global/GlobalSvgSelector";
+import { GlobalSvgSelector } from '../../../../assets/icons/global/GlobalSvgSelector'
+import { Weather } from '../../../../store/types/types'
 
-export const ThisDay = () => {
-  return (
-    <div className={styles.thisDay}>
-      <div className={styles.thisDay__topBlock}>
-        <div className={styles.wrapper}>
-          <div className={styles.thisDay__topBlock_temp}>20°</div>
-          <div className={styles.thisDay__topBlock_day}>Today</div>
-        </div>
-        <GlobalSvgSelector id='sunny' />
-      </div>
-      <div className={styles.thisDay__bottomBlock}>
-        <div className={styles.thisDay__bottomBlock_time}>Time: <span>21:54</span></div>
-        <div className={styles.thisDay__bottomBlock_city}>City: <span>Kremenchug</span></div>
-      </div>
-    </div>
-  )
+interface ThisDayProps {
+	weather: Weather
+}
+
+export const ThisDay = ({ weather }: ThisDayProps) => {
+	return (
+		<div className={styles.thisDay}>
+			<div className={styles.thisDay__topBlock}>
+				<div className={styles.wrapper}>
+					<div className={styles.thisDay__topBlock_temp}>{Math.round(weather.main.temp)}°</div>
+					<div className={styles.thisDay__topBlock_day}>Today</div>
+				</div>
+				<GlobalSvgSelector id='sunny' />
+			</div>
+			<div className={styles.thisDay__bottomBlock}>
+				<div className={styles.thisDay__bottomBlock_time}>Time: <span>21:54</span></div>
+				<div className={styles.thisDay__bottomBlock_city}>City: <span>Kremenchug</span></div>
+			</div>
+		</div>
+	)
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,27 @@
-import styles from './Home.module.scss';
-import {ThisDay} from "./components/ThisDay";
-import {ThisDayInfo} from "./components/ThisDayInfo";
-import {Days} from "./components/Days/Days";
+import styles from './Home.module.scss'
+import { ThisDay } from './components/ThisDay'
+import { ThisDayInfo } from './components/ThisDayInfo'
+import { Days } from './components/Days/Days'
+import { useCustomDispatch, useCustomSelector } from '../../hooks/store'
+import { useEffect } from 'react'
+import { fetchCurrentWeather } from '../../store/thunks/fetchCurrentWeather'
+import { selectCurrentWeatherData } from '../../store/selectors'
 
 export const Home = () => {
-  return (
-    <div className={styles.home}>
-      <div className={styles.wrapper}>
-        <ThisDay />
-        <ThisDayInfo />
-      </div>
-      <Days />
-    </div>
-  )
+	const dispatch = useCustomDispatch()
+
+	const { weather } = useCustomSelector(selectCurrentWeatherData)
+
+	useEffect(() => {
+		dispatch(fetchCurrentWeather('kremenchug'))
+	}, [])
+	return (
+		<div className={styles.home}>
+			<div className={styles.wrapper}>
+				<ThisDay weather={weather} />
+				<ThisDayInfo />
+			</div>
+			<Days />
+		</div>
+	)
 }
